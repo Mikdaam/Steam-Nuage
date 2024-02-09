f4928785ee03de72075402b0558349000d55e3e8
Mick <bmikdaam@gmail.com>
2024-02-09 16:33:33 +0100

    Initial commit : Begin the API implementation

---
+++ src/main/java/fr/side/projects/steamnuage/SteamNuageApplication.java
@@ -0,0 +1,13 @@
+package fr.side.projects.steamnuage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamNuageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamNuageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/side/projects/steamnuage/SteamNuageApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.side.projects.steamnuage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamNuageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
