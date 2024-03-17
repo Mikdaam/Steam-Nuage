82e14780d2ad3776b484bee91ac3dcf3c8ffd19c
Mick <bmikdaam@gmail.com>
2024-03-17 20:29:16 +0100

    feat : add id to player model

--- src/main/java/fr/side/projects/steamnuage/repositories/PlayerRepository.java
+++ src/main/java/fr/side/projects/steamnuage/repositories/PlayerRepository.java
@@ -3,5 +3,5 @@ package fr.side.projects.steamnuage.repositories;
 import fr.side.projects.steamnuage.models.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlayerRepository extends JpaRepository<Player, String> {
+public interface PlayerRepository extends JpaRepository<Player, Long> {
 }
